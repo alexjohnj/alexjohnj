3e6bd8604197d42c07abc64558bca3b5148da38a
Alex Jackson <alex@alexj.me>
2013-01-02 15:13:51 +0000

    Added a small deployment script
    The script generates the website, gzips html, css and javascript assets and then uploads it to S3.

---
+++ _deploy.py
@@ -0,0 +1,96 @@
+#! /usr/local/bin/python3
+
+import subprocess
+import sys
+import gzip
+import os
+
+try:
+    import yaml
+except ImportError:
+    sys.exit("PyYaml not available, Exiting")
+    
+def get_s3_bucket_name():
+    """
+    Opens an _config.yml file that is in the root of the directory that the script is in and return the value of the s3bucket key
+    
+    Returns: String 
+    
+    """
+    try:
+        with open("_config.yml") as f:
+            config_file = yaml.load(f)
+            try:
+                return config_file["s3bucket"]
+            except KeyError:
+                sys.exit("Error, no s3 bucket was specified in _config.yml. Make sure you add a \"s3bucket\" key to your _config.yml file.")
+                
+    except IOError as e:
+        sys.exit("I/O error({0}): {1}".format(e.errno, e.strerror))
+        
+def generate_site():
+    """
+    Runs jekyll --no-auto to generate the site.
+    
+    Returns: None
+    
+    """    
+    try: 
+        subprocess.check_call(["jekyll", "--no-auto"])
+    except subprocess.CalledProcessError:
+        sys.exit("Something went wrong generating the site")
+        
+def gzip_files():
+    for root, dirs, files in os.walk("_site/"): # Traverse the _site/ directory
+        for f in files:
+            filename, extension = os.path.splitext(f) # Get the extension of the current file
+            if extension == ".html" or extension == ".css" or extension == ".js": # Check to see if it's a compressible extension
+                with open(os.path.join(root, f), 'r') as f_to_compress: # Open the file
+                    with gzip.open(os.path.join(root, f) + ".gz", 'wb') as f_compressed: # Open a new gzip file
+                        f_compressed.writelines(f_to_compress) # Write the original file into the gzip file
+                os.remove(os.path.join(root, f)) # Remove the original file. 
+                os.rename(os.path.join(root, f) + ".gz", os.path.join(root, f)) # Rename file to original
+            
+        
+def deploy_to_s3(bucket):
+    """
+    Runs s3cmd sync -P _site/ s3://bucket-name to deploy the site to Amazon S3
+    
+    Key Arguments:
+    
+    bucket -- The name of the s3 bucket to upload to in the format s3://bucket-name
+    
+    Returns: None
+    
+    """
+    try: # Upload all uncompressed files
+        print("Uploading uncompressed files")
+        command = "s3cmd sync -P --exclude '*.html' --exclude '*.js' --exclude '*.css' _site/ " + bucket
+        subprocess.call(command, shell=True)
+    except subprocess.CalledProcessError:
+        sys.exit("Something went wrong deploying the site to s3.")
+    
+    try: # Upload all compressed files and add an appropriate header
+        print("Uploading compressed files")
+        command = "s3cmd sync -P --add-header='Content-Encoding: gzip' --exclude '*.*' --include '*.html' --include '*.js' --include '*.css' _site/ " + bucket
+        subprocess.check_call(command, shell=True)
+        
+    except subprocess.CalledProcessError:
+        sys.exit("Something went wrong setting the content encoding on the files deployed to s3.")
+
+if __name__ == "__main__":    
+    print("**************\nRunning Jekyll\n**************")
+    generate_site()
+    
+    print("**********\nGzipping Files\n**************")
+    gzip_files()
+    print("**********\nSuccessfully Compressed Files\n*************")
+    
+    print("*******************\nGetting Bucket Name\n*******************")
+    bucket_name = get_s3_bucket_name()
+    print("Success, will upload to bucket \033[94m%s\033[0m" % bucket_name)
+    
+    print("***************\nDeploying to s3\n***************")
+    deploy_to_s3(bucket_name)
+    print("**************************\nSuccessfully Deployed Site\n**************************")
+   
