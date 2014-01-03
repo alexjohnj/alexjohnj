8adf8e1fdc4a1f57bde401a82b1ce31a0d450c36
Alex Jackson <alex@alexj.me>
2014-01-03 00:47:06 +0000

    Started using jekyll-asset-pipeline plugin

---
+++ _plugins/jekyll_asset_pipeline.rb
@@ -0,0 +1,15 @@
+require 'jekyll_asset_pipeline'
+
+module JekyllAssetPipeline
+  class SassConverter < JekyllAssetPipeline::Converter
+    require 'sass'
+
+    def self.filetype
+      '.scss'
+    end
+
+    def convert
+      return Sass::Engine.new(@content, syntax: :scss, style: :compressed, :load_paths => ["_assets/styles/sass/"]).render
+    end
+  end
+end
