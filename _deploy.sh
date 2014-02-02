70133e1904c40e9978acca4170c0b90c9fccdbc2
Alex Jackson <alex@alexj.me>
2014-02-02 18:36:53 +0000

    Converted _deploy to a shell script

---
+++ _deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/local/bin/fish
+
+bundle exec jekyll build
+
+htmlcompressor -r -o _site _site
+
+find _site -type f -name '*.html' -exec gzip -9k '{}' \;
+find _site -type f -name '*.css' -exec gzip -9k '{}' \;
+find _site -type f -name '*.js' -exec gzip -9k '{}' \;
+
+rsync -avz -e 'ssh -p 49876' --delete-after _site alex@95.85.60.211:/var/www/alexj.me/_site
