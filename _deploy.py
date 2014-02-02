477b3dc1cfa5f27260de216501eebdc84ac906ba
Alex Jackson <alex@alexj.me>
2014-02-02 18:06:00 +0000

    Updated deploy script for deployment to VPS

--- _deploy.py
+++ _deploy.py
@@ -1,101 +1,75 @@
 #! /usr/local/bin/python3
-""" Requirements: s3cmd, jekyll, gzip & sass. 
-
-Mk 2 of the script
-
-To use this script, you'll need to edit your site's _config.yml file and add the following:
-
-s3bucket: s3://bucket-name
-
-Remember: Change the path_to_sass_file & sass_compile_path variables if you want to compile sass files. If you don't want to compile sass files, comment out the call to the compile_sass() function.
-
+""" 
+1. Build Jekyll site
+2. Minify HTML (CSS & JS already implemented in asset pipeline)
+3. Create gziped files
 """
 
 import gzip
+import getpass
 import os
-from subprocess import PIPE, check_call, CalledProcessError
-from sys import exit
-from time import sleep
-import argparse
+import sys
+import subprocess
+import shlex
+import pexpect
 
-def get_s3_bucket_name():
-    with open("_config.yml") as f:
-        for line in f:
-            if line.split(':')[0] == "s3bucket":
-                return line.strip("s3bucket:").strip() # Remove the s3bucket: part of the line and any white space
-        exit("Error: No bucket was found in the site's _config.yml file")
-        
-def generate_site():
+# Colours for output
+
+INFO = '\033[95m'
+ERROR = '\033[91m'
+OK = '\033[92m'
+NORM = '\033[0m'
+
+# Deployment variables
+local_build_directory = '_site/'
+remote_deploy_directory = '/var/www/alexj.me/_site/'
+ssh_user = 'alex'
+ssh_ip = "95.85.60.211"
+ssh_port = 49876
+
+def build_jekyll_site():
+    command = ['bundle', 'exec', 'jekyll', 'build']
+    try: 
+        subprocess.check_call(command, stdout=subprocess.PIPE)
+    except subprocess.CalledProcessError:
+        sys.exit(ERROR + "[Error]: Jekyll build failed" + NORM)
+    else:
+        print(OK + "Done" + NORM)
+
+def minify_files():
+    command = ['htmlcompressor', '-r', '-o', local_build_directory, local_build_directory]
     try:
-        check_call(["bundle", "exec", "jekyll", "build"], stdout=PIPE)
-    except CalledProcessError:
-        exit("Something went wrong generating the site with Jekyll.")
-        
+        subprocess.check_call(command, stdout=subprocess.PIPE)
+    except subprocess.CalledProcessError:
+        sys.exit(ERROR + "[Error]: Failed to minify HTML files" + NORM)
+    else:
+        print(OK + "Done" + NORM)
+
 def gzip_files():
-    for root, dirs, files in os.walk("_site/"): # Traverse the _site/ directory
-        for f in files:
-            if os.path.splitext(f)[1] in ['.html', '.css', '.js']:
-                current_path = os.path.join(root, f)
-                with open(current_path, 'rb') as f_in:
-                    with gzip.open(current_path + '.gz', 'wb') as f_out:
-                        f_out.writelines(f_in)
-                os.replace(current_path + '.gz', current_path)
-        
-def deploy_to_s3_bucket(bucket, dry_run=False):
-    if dry_run:
-        print("Doing a dry run!")
+    command = ['gzip', '-r', '-k', '--best', local_build_directory]
+    try:
+        subprocess.check_call(command, stdout=subprocess.PIPE)
+    except subprocess.CalledProcessError:
+        sys.exit(ERROR + "[Error]: Failed to gzip output files" + NORM)
+    else:
+        print(OK + "Done" + NORM)
 
-    try: # Upload all uncompressed files
-        command = "s3cmd sync -P --exclude '*.html' --exclude '*.js' --exclude '*.css' _site/ " + bucket
-        if dry_run:
-            command_ls = command.split()
-            command_ls.insert(-2, "--dry-run")
-            command = ' '.join(command_ls)
-
-        check_call(command, shell=True, stdout=PIPE)
-    except CalledProcessError:
-        exit("Something went wrong deploying the site to s3.")
-    
-    try: # Upload all compressed files and add an appropriate header
-        command = "s3cmd sync -P --add-header='Content-Encoding: gzip' --exclude '*.*' --include '*.html' --include '*.js' --include '*.css' _site/ " + bucket
-        if dry_run:
-            command_ls = command.split()
-            command_ls.insert(-2, "--dry-run")
-            command = ' '.join(command_ls)
-
-        check_call(command, shell=True, stdout=PIPE)
-    except CalledProcessError:
-        exit("Something went wrong setting the content encoding on the files deployed to s3")
-
-    try: # Remove any files that have been deleted
-        command = "s3cmd sync -P --delete-removed _site/ " + bucket
-        if dry_run:
-            command_ls = command.split()
-            command_ls.insert(-2, "--dry-run")
-            command = ' '.join(command_ls)
-        check_call(command, shell=True, stdout=PIPE)
-    except CalledProcessError:
-        exit("Something went wrong removing files from the bucket")
+def deploy_site():
+    command = "rsync -avz -e 'ssh -p {0}' --delete-after {1} {2}@{3}:{4}".format(ssh_port, local_build_directory, ssh_user, ssh_ip, remote_deploy_directory)
+    child = pexpect.spawn(command)
+    child.expect("{0}@{1}'s password:".format(ssh_user, ssh_ip))
+    child.sendline(getpass.getpass("{0}@{1}'s password:".format(ssh_user, ssh_ip)))
+    child.interact()
 
 if __name__ == "__main__":
+    print(INFO + "Building Jekyll Site..." + NORM)
+    build_jekyll_site()
 
-    parser = argparse.ArgumentParser(description="Deploy a Jekyll site to Amazon S3")
-    parser.add_argument('-n', '--dry-run', help="Perform a dry run when deploying to S3 (akin to running s3cmd with the --dry-run flag)", action='store_true')
-    args = parser.parse_args()
-    
-    print("Generating Site")
-    generate_site()
-    
-    print("Gzipping Files...")
+    print(INFO + "Minifying HTML files..." + NORM)
+    minify_files()
+
+    print(INFO + "Gzipping contents of {0}...".format(local_build_directory) + NORM)
     gzip_files()
-    
-    print("Getting Bucket Name...")
-    bucket_name = get_s3_bucket_name()
 
-    for i in reversed(range(1,4)):
-        print("\rDeploying to {0} in {1}".format(bucket_name, i), end='')
-        sleep(1)
-
-    print("\nDeploying...")
-    deploy_to_s3_bucket(bucket_name, dry_run=args.dry_run)
-    print("Successfully Deployed Site!")
+    print(INFO + "Deploying site via rsync..." + NORM)
+    deploy_site()
