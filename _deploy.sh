c2b68ded17eb2112f6b381335b775cfadc00c6e3
Alex Jackson <alex@alexj.me>
2014-02-03 00:20:00 +0000

    Fixed deploy script.

--- _deploy.sh
+++ _deploy.sh
@@ -8,4 +8,4 @@ find _site -type f -name '*.html' -exec gzip -9k '{}' \;
 find _site -type f -name '*.css' -exec gzip -9k '{}' \;
 find _site -type f -name '*.js' -exec gzip -9k '{}' \;
 
-rsync -avz -e 'ssh -p 49876' --delete-after _site alex@95.85.60.211:/var/www/alexj.me/_site
+rsync -avz -e 'ssh -p 49876' --delete-after _site/ alex@95.85.60.211:/var/www/alexj.me/_site/
