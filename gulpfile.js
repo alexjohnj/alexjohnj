cdba6688512d652c615e1c7950e090088190104f
Alex Jackson <alex@alexj.org>
2014-08-26 17:43:39 +0100

    Updated Gulpfile to handle scripts
    Will minify *.js and compile and minify *.coffee

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // Plugins
 var gulp = require('gulp'),
+    gutil = require('gulp-util'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
@@ -8,7 +9,10 @@ var gulp = require('gulp'),
     exec = require('child_process').exec,
     gzip = require('gulp-gzip'),
     rsync = require('rsyncwrapper').rsync,
-    htmlmin = require('gulp-htmlmin');
+    htmlmin = require('gulp-htmlmin'),
+    sourcemaps = require('gulp-sourcemaps'),
+    coffee = require('gulp-coffee'),
+    uglify = require('gulp-uglify');
 
 // Build SASS stylesheets, run the built sheet through autoprefixer and then minify it
 gulp.task('styles', function() {
@@ -20,9 +24,40 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('./static/css'));
 });
 
+// Compiles CoffeeScript with a sourcemap and saves it as *.min.js
+// Despite the .min suffix, this JS is NOT minified
+gulp.task('scripts-dev', function() {
+  return gulp.src('./static/js/*.coffee')
+    .pipe(coffee().on('error', gutil.log))
+    .pipe(sourcemaps.write())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./static/js'))
+});
+
+// Minify *.js, compile *.coffee, minify output, save as *.min.js
+gulp.task('scripts-product', ['clean-scripts'], function(cb) {
+  // Compile & Minify CoffeeScript
+  gulp.src('./static/js/*.coffee')
+    .pipe(coffee().on('error', gutil.log))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./static/js'))
+
+  // Minify JS
+  gulp.src('./static/js/**/*.js')
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./static/js'))
+});
+
 // Clean out the ./public/ directory
 gulp.task('clean', function(cb) {
   del(['./public/*'], cb);
+});
+
+// Deletes all *.min.js files
+gulp.task('clean-scripts', function(cb) {
+  del(['./static/js/**/*.min.js'], cb)
 });
 
 // Run hugo and rebuild the site
@@ -71,16 +106,17 @@ gulp.task('rsync', ['hugo', 'htmlmin', 'gzip'], function(cb) {
 // Watch for changes
 gulp.task('watch', function() {
   gulp.watch('./static/css/**/*.scss', ['styles']);
+  gulp.watch('./static/js/**/*.coffee', ['scripts-dev'])
 });
 
 // Default task
-gulp.task('default', ['clean', 'styles'], function() {
+gulp.task('default', ['clean', 'styles', 'scripts-product'], function() {
   gulp.start('hugo');
   gulp.start('htmlmin'); // Will not execute until 'hugo' task is done
   gulp.start('gzip'); // Will not execute until 'hugo' and 'htmlmin' tasks are done
 });
 
-gulp.task('deploy', ['clean', 'styles'], function() {
+gulp.task('deploy', ['clean', 'styles', 'scripts-product'], function() {
   gulp.start('hugo');
   gulp.start('htmlmin');
   gulp.start('gzip');
