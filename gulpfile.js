c71c8fb4ed7b3bc5476af409125f07fe69e67d52
Alex Jackson <alex@alexj.org>
2015-08-11 18:41:25 +0100

    Rewrite Gulpfile
    The file should be much easier to extend now. Major changes include:
    
    - Source directories are no longer polluted with compiled and minified
      files. All output files go in the public folder.
    - Explicit task run ordering using the run-sequence plugin.
    - Generation of source maps for the development CSS and CoffeeScript
      tasks.
    - The watch task now monitors all files and rebuilds the site with hugo
      when changed. Running hugo server is (almost) no longer necessary.
    
    The watch task still needs updating to start a server and enable live
    reloading in browsers.

--- gulpfile.js
+++ gulpfile.js
@@ -12,88 +12,71 @@ var gulp = require('gulp'),
     htmlmin = require('gulp-htmlmin'),
     sourcemaps = require('gulp-sourcemaps'),
     coffee = require('gulp-coffee'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    runSequence = require('run-sequence')
+    sourcemaps = require('gulp-sourcemaps');
 
-/******************************************************************************
-                              Source Compilation
-******************************************************************************/
-
-// styles-dev compiles main.scss, runs it through autoprefixer and saves
-// the output as main.min.css. The file is NOT minified however.
-gulp.task('styles-dev', function() {
-  return gulp.src('./static/css/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer('last 2 versions', 'ie 9'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./static/css'))
-});
-
-// styles-product compiles main.scss, runs it through autoprefixer, minifies
-// the output and saves it to the file main.min.css
-gulp.task('styles-product', function () {
-  return gulp.src('./static/css/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer('last 2 versions', 'ie 9'))
-    .pipe(minifycss())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./static/css'))
-});
-
-// coffee-dev compiles *.coffee in the js folder and saves the output
-// to *.min.js. The output is NOT minified however.
-gulp.task('coffee-dev', function() {
-  return gulp.src('./static/js/*.coffee')
-    .pipe(coffee().on('error', gutil.log))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./static/js'));
-});
-
-// coffee-product compiles *.coffee in the js folder, minifies the output
-// and saves it to *.min.js.
-gulp.task('coffee-product', function() {
-  return gulp.src('./static/js/*.coffee')
-    .pipe(coffee().on('error', gutil.log))
-    .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./static/js'));
-});
-
-// js-dev matches any file ending in .js but not .min.js and saves it as
-// *.min.js. It does NOT minify the file though.
-gulp.task('js-dev', function() {
-  return gulp.src('./static/js/**/!(*.min.js)+(*.js)')
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./static/js'));
-});
-
-// js-product matches any file ending in .js but not .min.js, minifies it
-// and then saves the output as *.min.js.
-gulp.task('js-product', function() {
-  return gulp.src('./static/js/**/!(*.min.js)+(*.js)')
-    .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./static/js'));
-});
-
-/******************************************************************************
-                            Static Site Generation
-******************************************************************************/
-
-// clean deletes everything in the ./public/ directory
-gulp.task('clean', function(cb) {
-  return del(['./public/*'], cb);
-});
-
-// hugo runs the hugo command and builds the site
-gulp.task('hugo', function(cb){
-  return exec('hugo', function(err) {
-    if (err) return cb(err);
+/*******************************************************************************
+                          Source Compilation
+*******************************************************************************/
+gulp.task('development:hugo-build', function(cb) {
+  return exec('hugo --baseUrl="http://localhost:8000"', function(err) {
+    if(err) return cb(err);
     cb();
   });
 });
 
-// htmlmin minifies ./public/**/*.html and overwrites the original file
-gulp.task('htmlmin', ['hugo'], function() {
+gulp.task('production:hugo-build', function(cb) {
+  return exec('hugo', function(err) {
+    if(err) return cb(err);
+    cb();
+  });
+});
+
+gulp.task('development:styles', function (){
+  return gulp.src('./public/css/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version', 'ie 9'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('production:styles', function() {
+  return gulp.src('./public/css/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 versions', 'ie 9'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('development:coffee-script', function() {
+  return gulp.src('./public/js/*.coffee')
+    .pipe(sourcemaps.init())
+    .pipe(coffee().on('error', gutil.log))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('production:coffee-script', function() {
+  return gulp.src('./public/js/*.coffee')
+    .pipe(coffee().on('error', gutil.log))
+    .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('development:javascript', function() {/*Dummy task*/});
+
+gulp.task('production:javascript', function() {
+    return gulp.src('./public/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js'));
+});
+
+/*******************************************************************************
+                           HTML Processing
+*******************************************************************************/
+
+gulp.task('production:html', function() {
   return gulp.src('./public/**/*.html')
     .pipe(htmlmin({
       collapseWhitespace: true,
@@ -106,21 +89,18 @@ gulp.task('htmlmin', ['hugo'], function() {
     .pipe(gulp.dest('./public'));
 });
 
-// clean-deploy removes all coffeescript, SASS and unminified JavaScript
-// files that get copied into the public/ folder during hugo's build.
-gulp.task('clean-deploy', ['hugo', 'htmlmin'], function(cb) {
-  return del(['./public/**/*.{scss,coffee,map}', './public/**/!(*.min.js)+(*.js)'], cb)
-});
-
-// gzip compresses the contents of ./public/**/*.{html,js,css} and saves the
-// output to *.{html,js,css}.gz
-gulp.task('gzip', ['hugo', 'htmlmin', 'clean-deploy'], function() {
+gulp.task('gzip', function() {
   return gulp.src('./public/**/*.{html,js,css,svg,xml}')
     .pipe(gzip())
     .pipe(gulp.dest('./public'));
+  cb();
 });
 
-gulp.task('rsync', ['hugo', 'htmlmin', 'clean-deploy', 'gzip'], function() {
+gulp.task('clean-build-dir', function(cb) {
+  del(['./public/**/*.{scss,coffe,map}', './public/**/*.coffee'], cb);
+});
+
+gulp.task('rsync', function() {
   return rsync({
     ssh: true,
     src: './public/',
@@ -133,34 +113,45 @@ gulp.task('rsync', ['hugo', 'htmlmin', 'clean-deploy', 'gzip'], function() {
   });
 });
 
-/******************************************************************************
-                                Main Tasks
-******************************************************************************/
+/*******************************************************************************
+                              Main Tasks
+*******************************************************************************/
 
-// default does everything except sync the public folder to the remote server.
-gulp.task('default', ['clean', 'styles-product', 'coffee-product', 'js-product'], function() {
-  gulp.start('hugo');
-  gulp.start('htmlmin');
-  gulp.start('clean-deploy');
-  gulp.start('gzip');
+gulp.task('clean', function(cb) {
+  del(['./public/*'], cb)
 });
 
-// deploy does everything including syncing the public folder to the remote server.
-gulp.task('deploy', ['clean', 'styles-product', 'coffee-product', 'js-product'], function() {
-  gulp.start('hugo');
-  gulp.start('htmlmin');
-  gulp.start('clean-deploy');
-  gulp.start('gzip');
-  gulp.start('rsync');
+gulp.task('development:build', function(cb) {
+  runSequence('clean',
+              'development:hugo-build',
+              'development:coffee-script',
+              ['development:styles', 'development:javascript'],
+              cb);
+  
 });
 
-/******************************************************************************
-                                Watch Tasks
-******************************************************************************/
-
-gulp.task('watch', ['clean'], function() {
-  gulp.watch('./static/css/**/*.scss', ['styles-dev']);
-  gulp.watch('./static/js/**/!(*.min.js)+(*.js)', ['js-dev']);
-  gulp.watch('./static/js/**/*.coffee', ['coffee-dev']);
+gulp.task('production:build', function(cb) {
+  runSequence('clean',
+              'production:hugo-build',
+              'production:coffee-script',
+              ['production:styles', 'production:javascript'],
+              'production:html',
+              'gzip',
+              'clean-build-dir',
+              cb);
 });
 
+gulp.task('default', ['production:build']);
+
+gulp.task('deploy', function() {
+  runSequence('production:build',
+              'rsync');
+});
+
+gulp.task('watch', ['development:build'], function() {
+  gulp.watch(['./archetypes/**/*.*', './content/**/*.*', './static/**/*.*',
+              './layouts/**/*.*', './config.toml'], ['development:hugo-build']);
+  gulp.watch('./public/css/**/*.scss', ['development:styles']);
+  gulp.watch('./public/js/**/*.coffee', ['development:coffee-script']);
+  gulp.watch('./public/js/**/*.js', ['development:javascript']);
+});
